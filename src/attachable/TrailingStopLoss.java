75b781d0d504ecf5059836817a41c00fb7cda0d9
rifaiad <rifaiad2000@gmail.com>
2022-09-26 12:45:31 +0200

    Fixed the TrailingStopLoss closing a trade within seconds of its opening.

--- src/attachable/TrailingStopLoss.java
+++ src/attachable/TrailingStopLoss.java
@@ -17,11 +17,16 @@ public class TrailingStopLoss extends Attachable implements Runnable {
     @Override
     public void run()   {
         float highest = 0;
+        try {
+            Thread.sleep(20000);
+        } catch (Exception ignored) {
+
+        }
         while(trade.isOpen())   {
             float close = Float.parseFloat(array.getLastInstantCandlestick().getClose());
             if (close > highest)
                 highest = close;
-            float boundary = (float) (highest - (0.15 * highest) / 100);
+            float boundary = (float) (highest - (0.25 * highest) / 100);
             if (Float.parseFloat(array.getLastInstantCandlestick().getClose()) < boundary)   {
                 try {
                     controller.sellSignal();
